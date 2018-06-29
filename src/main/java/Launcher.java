46d7051f18812ed7ebff23f0a53878d76a58fd7c
Øyvind Strømmen <insula.ventus@gmail.com>
2018-06-29 11:00:09 +0200

    Initial commit

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,9 @@
+public class Launcher {
+
+    public static void main(String [] args) {
+        System.out.println("Launching a game of blackjack");
+        if (args.length > 0) {
+            System.out.println(String.format("With params: %s", args[0]));
+        }
+    }
+}
